54ca758e5d8c17de25b53626eae7aca98edd15b9
christmknd <christmokonda@gmail.com>
2022-09-06 11:43:50 +0200

    Fonction Display

--- IntroTpFinal/intro.go
+++ IntroTpFinal/intro.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+type Coors struct {
+	x, y int
+}
+
+type ship struct {
+	coors       Coors
+	orientation int
+	size        int
+	hits        []Coors
+}
+
+func display() {
+	var i, j int
+	a := 0
+	l := 10
+
+	for j := 0; j < 10; j++ {
+		a++
+		fmt.Printf(" %d", a)
+	}
+	a = l
+	fmt.Printf("\n")
+	for i = 0; i < l; i++ {
+		fmt.Printf("|")
+		for j = 0; j < 10; j++ {
+			fmt.Printf(" |")
+		}
+		fmt.Printf("\n")
+	}
+	fmt.Printf("\n")
+}
+
+func main() {
+	display()
 }
