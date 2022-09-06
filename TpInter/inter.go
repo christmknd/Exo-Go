b84f074924a21aaf55c7f824b4b1751e8310f120
christmknd <christmokonda@gmail.com>
2022-09-06 15:13:35 +0200

    ADD '/' Route

--- TpInter/inter.go
+++ TpInter/inter.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
+func ApiHandler(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		t := time.Now()
+		hour := t.Hour()
+		minute := t.Minute()
+		fmt.Fprintf(w, "%v h %v", hour, minute)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", ApiHandler)
+	http.ListenAndServe(":4567", nil)
 }
